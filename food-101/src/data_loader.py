72b5bc1950e87a08295f78c5974298a1f912cfdb
Yuanxyyds <liuhongyuan.liu@mail.utoronto.ca>
2024-09-23 21:08:40 +0000

    feat: add vgg, inception, resnet models and reformat code

--- food-101/src/data_loader.py
+++ food-101/src/data_loader.py
@@ -1,3 +1,27 @@
+"""
+This script provides utility functions to manage and visualize the Food-101 dataset for a food classification model.
+
+Functions:
+    - visualize_dataset(): Visualizes a subset of the Food-101 dataset by displaying one image per class, organized in a grid.
+    - split_train_and_test_data(): Splits the raw data into separate training and testing directories based on predefined metadata files.
+    - split_mini_train_and_test_data(): Creates a smaller subset of the dataset by copying a specified list of food classes into new directories.
+
+Dependencies:
+    - os: For directory and file path manipulation.
+    - matplotlib.pyplot: For visualizing images from the dataset.
+    - numpy: For random selection of images.
+    - shutil: For copying files and directories.
+    - collections.defaultdict: For organizing and grouping images by class.
+    - config.Config: Imports dataset configuration and paths.
+
+Usage:
+    This script is intended to help prepare and manage the dataset for training a food classification model.
+    It can be used to visualize the dataset, split the raw data into training and testing sets, 
+    and create smaller, more manageable subsets of the data for testing and experimentation.
+
+Ensure the configuration in `config.Config` is properly set before running these functions.
+"""
+
 import os
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,8 +32,25 @@ from config import Config
 
 def visualize_dataset():
     """
-    Visualize the data, showing one image per class from 101 classes
+    Visualizes a subset of the dataset by displaying one image per class from the available food categories.
+
+    This function generates a grid of images, with each cell displaying a randomly selected image 
+    from one of the food categories present in the dataset. The number of rows and columns in the grid 
+    is determined by the `rows` and `cols` variables.
+
+    The output is saved as an image in the folder specified by `Config.PLOTS_DIR`.
+
+    Behavior:
+        - If the directory specified by `Config.PLOTS_DIR` does not exist, it is created.
+        - The images are displayed without axes ticks.
+        - The result is saved as a PNG file in `Config.PLOTS_DIR`.
+
+    Output:
+        - A saved image showing the visualization of food categories.
     """
+    # Make the plots folder if is not included
+    if not os.path.exists(Config.PLOTS_DIR):
+        os.makedirs(Config.PLOTS_DIR)
     rows = 17
     cols = 6
     _, ax = plt.subplots(rows, cols, figsize=(25, 25))
@@ -36,12 +77,24 @@ def visualize_dataset():
 
     plt.setp(ax, xticks=[], yticks=[])
     plt.tight_layout()
-    plt.savefig("food-101/plots/visualize_dataset.png")
+    plt.savefig(f"{Config.PLOTS_DIR}visualize_dataset.png")
 
 
 def split_train_and_test_data():
     """
-    Split the train and test data from the raw data folder
+    Splits the raw data into training and testing sets.
+
+    This function uses metadata files (`train.txt` and `test.txt`) to identify which images belong to 
+    the training and testing sets, respectively. The images are then copied from the source directory 
+    (`Config.DATA_DIR`) to their respective training and testing directories.
+
+    Behavior:
+        - Training images are copied to `Config.TRAIN_DATA_DIR`.
+        - Testing images are copied to `Config.TEST_DATA_DIR`.
+        - If the destination directories do not exist, they are created.
+
+    Output:
+        - The training and testing datasets are copied into their respective directories.
     """
 
     def extract_from_raw_data(filepath, src, dest):
@@ -80,7 +133,19 @@ def split_train_and_test_data():
 
 def split_mini_train_and_test_data():
     """
-    Select less classes of data from the splitted data
+    Creates smaller subsets of the training and testing data, using a limited number of food categories.
+
+    This function copies only the categories listed in `Config.FOOD_LIST` from the full training 
+    and testing sets to separate directories for faster prototyping and testing.
+
+    Behavior:
+        - The function removes any existing data in `Config.MINI_TRAIN_DATA_DIR` and `Config.MINI_TEST_DATA_DIR`.
+        - Copies images for the food categories specified in `Config.FOOD_LIST` into the mini train/test directories.
+        - Counts and logs the number of images copied into each directory.
+
+    Output:
+        - The mini training and testing datasets are copied into their respective directories.
+        - Logs the total number of images copied.
     """
 
     def extract_classes(food_list, src, dest) -> int:
@@ -110,7 +175,3 @@ def split_mini_train_and_test_data():
     )
     print("Completed with Training Dataset Size: ", Config.TRAIN_SAMPLE_SIZE)
     print("Completed with Testing Dataset Size: ", Config.TEST_SAMPLE_SIZE)
-
-
-if __name__ == "__main__":
-    split_mini_train_and_test_data()
--- food-101/src/model.py
+++ food-101/src/model.py
@@ -4,10 +4,32 @@ import numpy as np
 from keras.src.saving import load_model
 
 
-loaded_model = load_model("model_trained_3class.keras")
+def predict(model_path, image_path):
+    """
+    Loads a trained model and uses it to predict the class of a given image.
 
+    This function:
+    1. Loads a pre-trained model from the specified `model_path`.
+    2. Preprocesses the input image, resizing it to the dimensions required by the model.
+    3. Performs a prediction using the model and returns the predicted class from `Config.FOOD_LIST`.
 
-def predict_and_display(image_path):
+    Args:
+        model_path (str): The file path to the pre-trained model (in Keras format).
+        image_path (str): The file path to the image that will be classified.
+
+    Returns:
+        str: The predicted food class from `Config.FOOD_LIST`.
+
+    Example:
+        result = predict("path/to/model.h5", "path/to/image.jpg")
+        print(result)  # Outputs the predicted food class
+
+    Note:
+        The image is resized to the dimensions specified in `Config.IMAGE_HEIGHT` and `Config.IMAGE_WIDTH`
+        and normalized by dividing pixel values by 255.
+    """
+    # Load model
+    model = load_model(model_path)
     # Load and preprocess the image
     img = image.load_img(
         image_path, target_size=(Config.IMAGE_HEIGHT, Config.IMAGE_WIDTH)
@@ -17,11 +39,8 @@ def predict_and_display(image_path):
     img_array /= 255.0
 
     # Make predictions
-    predictions = loaded_model.predict(img_array)
+    predictions = model.predict(img_array)
     predicted_class = np.argmax(predictions)
 
     print(Config.FOOD_LIST[predicted_class])
-
-
-if __name__ == "__main__":
-    predict_and_display("apple-pie-www.thereciperebel.com-1200-17-of-53.jpg")
+    return Config.FOOD_LIST[predicted_class]
